59e11b52f802cebe2e5f3dedffd0a3138cd30898
clicksomething <sukaramer5@gmail.com>
2025-02-05 19:06:49 +0300

    updated initdb

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 // index.js
+const initDB = require('./database/initDB');
 const express = require('express');
 const app = express();
 
@@ -8,6 +9,11 @@ const userRoutes = require('./src/routes/userRoutes');
 // Use middleware to handle JSON requests
 app.use(express.json());
 
+initDB().then(() => {
+  console.log('Database tables created successfully!');
+}).catch((error) => {
+  console.error('Error creating database tables:', error);
+});
 // Use the user routes
 app.use('/api/users', userRoutes);  // All user-related routes
 
