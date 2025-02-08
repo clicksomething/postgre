c2b76eeb0abcb56ec59edeaa2040597484551e69
clicksomething <sukaramer5@gmail.com>
2025-02-08 14:09:44 +0300

    `Deleted ComponentPlan.js file`

---
+++ frontend/src/components/ComponentPlan.js
@@ -0,0 +1,25 @@
+const componentPlan = {
+  components: {
+    Login: "Already exists",
+    ForgotPassword: "Already exists",
+    Register: "New component for user registration",
+    Dashboard: "New component for displaying user information",
+    Profile: "New component for managing user profiles",
+    ExamManagement: {
+      CreateExam: "New component for creating exams",
+      ExamList: "New component for displaying all exams",
+      ExamDetail: "New component for viewing exam details",
+      EditExam: "New component for editing exams",
+      UploadExamSchedule: "New component for handling file uploads"
+    },
+    UserManagement: {
+      CreateUser: "New component for creating users",
+      UserList: "New component for displaying all users",
+      UserDetail: "New component for viewing user details",
+      EditUser: "New component for editing users"
+    },
+    ObserverManagement: "New components if required"
+  }
+};
+
+export default componentPlan;
